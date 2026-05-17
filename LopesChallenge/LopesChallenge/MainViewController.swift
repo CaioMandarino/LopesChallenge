32d0afd4035839174f4c3b968928c1dfd7a057c5
Caio <caiomand04@gmail.com>
2026-05-17 20:45:42 -0300

    added nadja image to view

--- LopesChallenge/LopesChallenge/MainViewController.swift
+++ LopesChallenge/LopesChallenge/MainViewController.swift
@@ -8,12 +8,31 @@
 import UIKit
 
 class MainViewController: UIViewController {
-
+    
+    var nadjaImage: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .systemPink
+        view.backgroundColor = .systemBackground
+        setNajdaImage()
     }
 
-
+    private func setNajdaImage() {
+        nadjaImage = UIImageView(image: .nadja)
+        nadjaImage.translatesAutoresizingMaskIntoConstraints = false
+        nadjaImage.contentMode = .scaleAspectFit
+        
+        view.addSubview(nadjaImage)
+        
+        let padding: CGFloat = 16
+        
+        NSLayoutConstraint.activate([
+            nadjaImage.widthAnchor.constraint(equalTo: view.widthAnchor, multiplier: 2 / 3),
+            nadjaImage.heightAnchor.constraint(equalTo: nadjaImage.widthAnchor),
+            nadjaImage.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: -padding),
+            nadjaImage.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: padding)
+        ])
+    }
+    
 }
 
