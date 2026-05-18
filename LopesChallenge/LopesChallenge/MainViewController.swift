2c5a3b898fa514aa2380766ba63b1311f3d3a7e1
Caio <caiomand04@gmail.com>
2026-05-17 21:00:55 -0300

    added nadja image name label

--- LopesChallenge/LopesChallenge/MainViewController.swift
+++ LopesChallenge/LopesChallenge/MainViewController.swift
@@ -10,27 +10,47 @@ import UIKit
 class MainViewController: UIViewController {
     
     var nadjaImage: UIImageView!
+    var imageNameLabel: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .systemBackground
         setNajdaImage()
+        setupImageNameLabel()
+    }
+    
+    private func setupImageNameLabel() {
+        imageNameLabel = UILabel()
+        imageNameLabel.translatesAutoresizingMaskIntoConstraints = false
+        imageNameLabel.textColor = .label
+        imageNameLabel.text = "Nadia e Eduardo Performando"
+        
+        view.addSubview(imageNameLabel)
+        
+        let padding: CGFloat = 16
+
+        NSLayoutConstraint.activate([
+            imageNameLabel.topAnchor.constraint(equalTo: nadjaImage.topAnchor),
+            imageNameLabel.leadingAnchor.constraint(equalTo: nadjaImage.trailingAnchor, constant: padding),
+            imageNameLabel.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -padding)
+            
+        ])
     }
 
     private func setNajdaImage() {
         nadjaImage = UIImageView(image: .nadja)
         nadjaImage.translatesAutoresizingMaskIntoConstraints = false
         nadjaImage.contentMode = .scaleAspectFit
-        
+
         view.addSubview(nadjaImage)
-        
-        let padding: CGFloat = 16
-        
+
+        let aspectRatio = nadjaImage.image!.size.height / nadjaImage.image!.size.width
+
         NSLayoutConstraint.activate([
             nadjaImage.widthAnchor.constraint(equalTo: view.widthAnchor, multiplier: 2 / 3),
-            nadjaImage.heightAnchor.constraint(equalTo: nadjaImage.widthAnchor),
-            nadjaImage.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: -padding),
-            nadjaImage.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: padding)
+            nadjaImage.heightAnchor.constraint(equalTo: nadjaImage.widthAnchor, multiplier: aspectRatio),
+            nadjaImage.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 10),
+            nadjaImage.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 10)
         ])
     }
     
